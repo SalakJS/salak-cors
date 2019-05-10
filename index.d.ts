819e3fec709540dd20c196ad55e100aa18c00101
wengeek <wenwei897684475@gmail.com>
2019-05-10 16:44:59 +0800

    fix: add - . : [ alias autoload bg bindkey break builtin bye cd chdir command compadd comparguments compcall compctl compdescribe compfiles compgroups compquote compset comptags comptry compvalues continue declare dirs disable disown echo echotc echoti emulate enable eval exec exit export false fc fg float functions getln getopts hash history integer jobs kill let limit local logout noglob popd print printf private pushd pushln pwd r read readonly rehash return sched set setopt shift source suspend test times trap true ttyctl type typeset ulimit umask unalias unfunction unhash unlimit unset unsetopt vared wait whence where which zcompile zformat zle zmodload zparseopts zregexparse zstyle option to d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@ import * as Salak from 'salak'
 declare module 'salak' {
   interface SalakConfig {
     cors: {
+      enable: boolean
       origin: string | Function
       allowMethos: string | string[]
       exposeHeaders: string | string[]
